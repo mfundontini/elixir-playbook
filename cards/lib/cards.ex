3dd4060fd408ca274aa1b0be12468efa639cf232
MfundoNtini <mfundontini@gmail.com>
2021-02-05 03:03:12 +0200

    FEATURE: Two possible ways of flattening our list

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -12,23 +12,37 @@ defmodule Cards do
       :world
 
   """
-  def create_deck() do
-    # Implicit return statements, explicit return causes a complilation error
+  def create_deck_meh() do
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 
-    for suit <- suits do
+    # For statements/list comprehensions are maps of a sort and each context will return a list, thus nested for.. statements will
+    # Produce nested lists. List.flatten recursively flattens lists, this is not the best solution. 
+    cards = for suit <- suits do
       
       for value <- values do
         "#{value} of #{suit}"
       end
 
     end
+
+    List.flatten(cards)
+  end
+
+  def create_deck() do
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+
+    # Nested comprehensions can also take this form where hierachy is from right to left, lowest to highest
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     # Function arguments are passed by value, and are thus immutable
     # Function signatures are of the form function/no_of_args eg shuffle/1
+    # Implicit return statements, explicit return causes a complilation error
     Enum.shuffle(deck)
   end
 
