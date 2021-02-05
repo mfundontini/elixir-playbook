b2ad6588768bf905cea6606d41f7a2d08d0860c8
MfundoNtini <mfundontini@gmail.com>
2021-02-05 02:36:18 +0200

    DOC: Added some comments for future reference.

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -13,14 +13,19 @@ defmodule Cards do
 
   """
   def create_deck() do
+    # Implicit return statements, explicit return causes a complilation error
     ["Ace of Spades", "Two of Hearts", "Three of Clubs", "Four of Diamonds", "Five of Spades"]
   end
 
   def shuffle(deck) do
+    # Function arguments are passed by value, and are thus immutable
+    # Function signatures are of the form function/no_of_args eg shuffle/1
     Enum.shuffle(deck)
   end
 
   def contains?(deck, card) do
+    # Standard library, is it called that, functions need not be imported or installed in any way
+    # Append ? to functions that return Booleans => convention!
     Enum.member?(deck, card)
   end
 end
