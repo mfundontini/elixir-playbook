e16697ffd1b84e791b248005b111a8a9d7ee49b6
MfundoNtini <mfundontini@gmail.com>
2021-02-06 09:35:00 +0200

    DOC: Added documentation for module and methods.

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,14 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+  Documentation for `Cards` module which is used for:
+   1. Creating a hand/deck of cards.
+   2. Shuffling a hand/deck cards.
+   3. Saving a hand/deck.
+   4. Loading a hand/deck.
+   5. Assessing if a hand/deck contains a specific card.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def create_deck_meh() do
+  defp create_deck_meh() do
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 
@@ -29,6 +25,29 @@ defmodule Cards do
     List.flatten(cards)
   end
 
+  @doc """
+  Creates a 52 card deck.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> deck
+      iex> deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades",
+      "Nine of Spades", "Ten of Spades", "Jack of Spades", "Queen of Spades",
+      "King of Spades", "Ace of Hearts", "Two of Hearts", "Three of Hearts",
+      "Four of Hearts", "Five of Hearts", "Six of Hearts", "Seven of Hearts",
+      "Eight of Hearts", "Nine of Hearts", "Ten of Hearts", "Jack of Hearts",
+      "Queen of Hearts", "King of Hearts", "Ace of Clubs", "Two of Clubs",
+      "Three of Clubs", "Four of Clubs", "Five of Clubs", "Six of Clubs",
+      "Seven of Clubs", "Eight of Clubs", "Nine of Clubs", "Ten of Clubs",
+      "Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds",
+      "Six of Diamonds", "Seven of Diamonds", "Eight of Diamonds",
+      "Nine of Diamonds", "Ten of Diamonds", "Jack of Diamonds", ...]
+
+  """
   def create_deck() do
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
@@ -39,6 +58,9 @@ defmodule Cards do
     end
   end
 
+  @doc """
+  Shuffles a deck of cards.
+  """
   def shuffle(deck) do
     # Function arguments are passed by value, and are thus immutable
     # Function signatures are of the form function/no_of_args eg shuffle/1
@@ -46,17 +68,42 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Checks if as card is contained within a deck.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> contained = Cards.contains?(deck, "Ace of Spades")
+      iex> contained
+      true
+
+  """
   def contains?(deck, card) do
     # Standard library, is it called that, functions need not be imported or installed in any way
     # Append ? to functions that return Booleans => convention!
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Deals a hand from the deck, where `count` is the number of crads on the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, _rest} = Cards.deal(deck, 2)
+      iex> hand
+      ["Ace of Spades", "Two of Spades"]
+
+  """
   def deal(deck, count) do
     # Returns a tuple
     Enum.split(deck, count)
   end
 
+  @doc """
+  Saves cards into a file with `filename` specified.
+  """  
   def save(deck, filename) do
     # File.write takes a filename and a chardata representation of the file, hence we are calling erlang's term_to_binary
     # Function to achieve this
@@ -69,6 +116,9 @@ defmodule Cards do
     end
   end
 
+  @doc """
+  Loads cards stored in a file with `filename` specified.
+  """  
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
@@ -76,6 +126,9 @@ defmodule Cards do
     end
   end
 
+  @doc """
+  Creates a `hand_size` amount of cards hand, which is randomized.
+  """  
   def create_hand(hand_size) do
     # Using the |> operator allows us to achieve the following lines of code
     # cards = Cards.create_deck()
