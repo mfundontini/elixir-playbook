393cf5acb86c2b9c7398148906986f2436ee0eff
MfundoNtini <mfundontini@gmail.com>
2021-02-10 14:21:33 +0200

    FEATURE: Added mirror rows method.

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,6 +4,7 @@ defmodule Identicon do
     input
     |> generate_hash
     |> get_colour
+    |> build_grid
   end
 
   def generate_hash(input) do
@@ -15,6 +16,19 @@ defmodule Identicon do
 
   def get_colour(%Identicon.Image{hex: [red, green, blue | _rest ]} = image) do
     %Identicon.Image{image | colour: {red, green, blue}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> mirror_rows
+  end
+
+  def mirror_rows(list) do
+    for row <- list do
+        [first, second, third] = row
+        [first, second, third, second, first]
+    end
   end
 
 end
