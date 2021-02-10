fcc8432d5640211e4d9c8cdc8a9594acbc2eec80
MfundoNtini <mfundontini@gmail.com>
2021-02-10 13:11:56 +0200

    FEATURE: Added generate hash method.

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,6 +1,12 @@
 defmodule Identicon do
   
   def main(input) do
-    
+    input
+    |> generate_hash
+  end
+
+  def generate_hash(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
